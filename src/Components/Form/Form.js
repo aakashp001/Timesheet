6fe8eff7426133265f37871d23c254a7341a22a8
Hari-118878 <e.haribabu7083@gmail.com>
2025-04-19 19:18:13 +0530

    commit

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Form.css'
+function Form() {
+  return (
+
+    <div className='form'>
+        <table>
+            <tr>
+                <th>First Name</th>
+            </tr>
+        </table>
+
+    </div>
+
+  )
+}
+
+export default Form
