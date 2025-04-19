a9bed0f49b239665953b1969c206dc54dfad714c
aakash <aakashcud02@gmail.com>
2025-04-19 16:22:52 +0530

    commit commit

--- src/Components/FirstPage/Idpage.js
+++ src/Components/FirstPage/Idpage.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Idpage.css';
 import Logo from '../../Assets/Img/CompanyLogo.webp';
 import Time from './Time/time';
 
-export default function idPage() {
+export default function IdPage() {
+  const [employeeId, setEmployeeId] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (employeeId === '001') {
+      alert('Welcome Aakash');
+    } else {
+      alert('Invalid ID');
+    }
+  };
+
   return (
-    <>
-      <div className='id-overall'>
-        
-<div className='id-box'>
-<img src={Logo} className='logo'></img>
-<Time/>
+    <div className='id-overall'>
+      <div className='id-box'>
+        <img src={Logo} className='logo' alt="Company Logo" />
+        <Time />
 
-<input type="text" id="id" name="id" placeholder='Enter Employer ID ' className='id-input'></input>
-
-<button>Login</button>
-</div>
-
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="id"
+            name="id"
+            placeholder="Enter Employer ID"
+            className="id-input"
+            value={employeeId}
+            onChange={(e) => setEmployeeId(e.target.value)}
+          />
+          <button type="submit">Login</button>
+        </form>
       </div>
-    </>
-  )
+    </div>
+  );
 }
